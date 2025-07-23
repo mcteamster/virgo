879effb9d210bd8116ce4445df613d9913246cda
mcteamster <28884260+mcteamster@users.noreply.github.com>
2025-07-23 19:57:34 +1000

    docs: update demo site

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -4,11 +4,11 @@ import { Virgo, Virgo2AWS } from '@mcteamster/virgo' // Production
 // import { Virgo, Virgo2AWS } from '../../src/index.ts' // Local Development
 
 function App() {
-  let timezone = 
+  let timezone =
     Intl.DateTimeFormat().resolvedOptions().timeZone
-    // 'Asia/Tokyo' // Override timezone for testing
+  // 'Asia/Tokyo' // Override timezone for testing
   const location = Virgo.getLocation(timezone)
-  const awsRegion = Virgo2AWS.getClosestRegion({origin: timezone})
+  const awsRegion = Virgo2AWS.getClosestRegion({ origin: timezone })
 
   const styles: { [key: string]: Properties<string | number> } = {
     body: {
@@ -18,9 +18,14 @@ function App() {
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
+      fontSize: 'calc(min(1vh, 2vw))'
     },
     title: {
       fontSize: '4.5em',
+    },
+    paragraph: {
+      fontSize: '1.75em',
+      padding: '0.5em',
     },
     subtitle: {
       fontSize: '1.5em',
@@ -37,24 +42,36 @@ function App() {
     value: {
       fontSize: '3em',
     },
+    link: {
+      color: 'white',
+    },
   }
 
   return (
     <div style={styles.body}>
       <div style={styles.title}>Virgo 📍</div>
-      <div style={styles.card}>
+      <div style={styles.paragraph}>
+        Approximate Location by Time Zone
+      </div>
+      <div style={styles.paragraph}>
+        <code>npm i @mcteamster/virgo</code>
+      </div>
+      <div style={{ ...styles.card }}>
         <div style={styles.subtitle}>Time Zone</div>
         <div style={styles.value}>{timezone}</div>
       </div>
-      <div style={styles.card} onClick={() => { window.open(`https://maps.apple.com/frame?center=${location.latitude}%2C${location.longitude}&span=4%2C10`, '_blank')}}>
+      <div style={{ ...styles.card, backgroundColor: '#66CDAA' }} onClick={() => { window.open(`https://maps.apple.com/frame?center=${location.latitude}%2C${location.longitude}&span=4%2C10`, '_blank') }}>
         <div style={styles.subtitle}>Centroid Coordinates</div>
         <div style={styles.value}>Latitude: {location.latitude}</div>
         <div style={styles.value}>Longitude: {location.longitude}</div>
       </div>
-      <div style={styles.card} onClick={() => { window.open('https://aws.amazon.com/about-aws/global-infrastructure/', '_blank')}}>
+      <div style={{ ...styles.card, backgroundColor: '#FF9900' }} onClick={() => { window.open('https://aws.amazon.com/about-aws/global-infrastructure/', '_blank') }}>
         <div style={styles.subtitle}>Closest AWS Region</div>
         <div style={styles.value}>{awsRegion.closestRegion}</div>
         <div style={styles.value}>~{awsRegion.distance.toFixed(3)} KM</div>
+      </div>
+      <div style={styles.paragraph}>
+        Learn more on <a style={styles.link} href="https://github.com/mcteamster/virgo">GitHub</a>
       </div>
     </div>
   )
