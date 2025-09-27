48a2aa97fae4df3ed1a13c0812e06af0417ff3fb
mcteamster <28884260+mcteamster@users.noreply.github.com>
2025-09-27 16:59:47 +1000

    docs: update demo styling

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -12,8 +12,10 @@ function App() {
 
   const styles: { [key: string]: Properties<string | number> } = {
     body: {
-      width: '100%',
-      height: '90vh',
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
@@ -21,7 +23,7 @@ function App() {
       fontSize: 'calc(min(1vh, 2vw))'
     },
     title: {
-      fontSize: '4.5em',
+      fontSize: '3.5em',
     },
     paragraph: {
       fontSize: '1.75em',
@@ -35,15 +37,14 @@ function App() {
       margin: '1em',
       padding: '1em',
       borderRadius: '1em',
-      backgroundColor: '#eee',
-      color: '#111',
+      border: '0.5em solid black',
       cursor: 'pointer',
     },
     value: {
       fontSize: '3em',
     },
     link: {
-      color: 'white',
+      color: 'black',
     },
   }
 
@@ -51,27 +52,27 @@ function App() {
     <div style={styles.body}>
       <div style={styles.title}>Virgo 📍</div>
       <div style={styles.paragraph}>
-        Approximate Location by Time Zone
+        Approximate Location from Time Zone
       </div>
-      <div style={styles.paragraph}>
-        <code>npm i @mcteamster/virgo</code>
-      </div>
-      <div style={{ ...styles.card }}>
-        <div style={styles.subtitle}>Time Zone</div>
+      <div style={{ ...styles.card, backgroundColor: '#DDD' }}>
+        <div style={styles.subtitle}>Your Time Zone</div>
         <div style={styles.value}>{timezone}</div>
       </div>
       <div style={{ ...styles.card, backgroundColor: '#66CDAA' }} onClick={() => { window.open(`https://maps.apple.com/frame?center=${location.latitude}%2C${location.longitude}&span=4%2C10`, '_blank') }}>
-        <div style={styles.subtitle}>Centroid Coordinates</div>
+        <div style={styles.subtitle}>Coordinate Estimate</div>
         <div style={styles.value}>Latitude: {location.latitude}</div>
         <div style={styles.value}>Longitude: {location.longitude}</div>
       </div>
       <div style={{ ...styles.card, backgroundColor: '#FF9900' }} onClick={() => { window.open('https://aws.amazon.com/about-aws/global-infrastructure/', '_blank') }}>
         <div style={styles.subtitle}>Closest AWS Region</div>
         <div style={styles.value}>{awsRegion.closestRegion}</div>
-        <div style={styles.value}>~{awsRegion.distance.toFixed(3)} KM</div>
+        <div style={styles.value}>{awsRegion.distance.toFixed(0)} KM</div>
       </div>
       <div style={styles.paragraph}>
         Learn more on <a style={styles.link} href="https://github.com/mcteamster/virgo">GitHub</a>
+      </div>
+      <div style={styles.paragraph}>
+        <code>npm i @mcteamster/virgo</code>
       </div>
     </div>
   )
